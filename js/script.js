89d8b7bda04219fb18ac159a5e039853db442521
Heather Nuffer <zzzuzz@gmail.com>
2016-10-27 11:15:12 -0600

    feat: Add stories from NY Times via its API

--- js/script.js
+++ js/script.js
@@ -23,6 +23,41 @@ function loadData() {
     var imgString = '<img class="bgimg" alt="Google street view of ' + location + '" src=' + streetviewUrl + '>';
     $body.prepend(imgString);
 
+
+    //load NY Times articles
+    var nytUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    nytUrl += '?' + $.param({
+      'api-key': "bc8fe802089b40a5a66c4ebb427cce69",
+      'q': city,
+      'sort': "newest",
+      'fl': "web_url,headline,lead_paragraph"
+    });
+
+    $.getJSON(nytUrl, function (data) {
+      var results = data.response.docs;
+      for (var i = 0; i < results.length; i++) {
+        var articleUrl = results[i].web_url;
+        var headline = results[i].headline.main;
+        var leadParagraph = results[i].lead_paragraph;
+
+        var $articleLi = $('<li>', {
+          "class": "article"
+          });
+
+        $('<a>', {
+          href: articleUrl,
+          text: headline,
+          target: "_new"
+          }).appendTo($articleLi);
+
+        $('<p>', {
+          text: leadParagraph
+          }).appendTo($articleLi);
+
+        $nytElem.append($articleLi);
+      }
+    })
+
     return false;
 };
 
