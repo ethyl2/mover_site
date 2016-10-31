feb4ca59ba099a95549d53021120b4a769d37c13
Heather Nuffer <zzzuzz@gmail.com>
2016-10-30 22:34:47 -0600

    feat: Add small google map of location

--- js/script.js
+++ js/script.js
@@ -173,6 +173,111 @@ function loadData() {
       clearTimeout(wikiRequestTimeout);
     });
 
+    /* Generate the custom Google Map for the website.
+    See the documentation below for more details.
+    https://developers.google.com/maps/documentation/javascript/reference
+    */
+
+    var map;    // declares a global map variable
+
+    function initializeMap() {
+
+      var mapOptions = {
+        disableDefaultUI: true,
+        scaleControl: true,
+      };
+
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      /*
+      createMapMarker(placeData) reads Google Places search results to create map pins.
+      placeData is the object returned from search results containing information
+      about a single location.
+      */
+      function createMapMarker(placeData) {
+
+        // The next lines save location data from the search result object to local variables
+        var lat = placeData.geometry.location.lat();  // latitude from the place service
+        var lon = placeData.geometry.location.lng();  // longitude from the place service
+        var name = placeData.formatted_address;   // name of the place from the place service
+        var bounds = window.mapBounds;            // current boundaries of the map window
+
+        // marker is an object with additional data about the pin for a single location
+        var marker = new google.maps.Marker({
+          map: map,
+          position: placeData.geometry.location,
+          title: name
+        });
+
+        // infoWindows are the little helper windows that open when you click
+        // or hover over a pin on a map. They usually contain more information
+        // about a location.
+        var infoWindow = new google.maps.InfoWindow({
+          content: name + ": Latitude: " + lat +
+          "<br>Longitude: " + lon
+          });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infoWindow.open(map,marker);
+        });
+
+        // this is where the pin actually gets added to the map.
+        // bounds.extend() takes in a map location object
+        bounds.extend(new google.maps.LatLng(lat, lon));
+        // fit the map to the new marker
+        map.fitBounds(bounds);
+        // center the map
+
+        map.setCenter(bounds.getCenter());
+      }
+
+      /*
+      callback(results, status) makes sure the search returned results for a location.
+      If so, it creates a new map marker for that location.
+      */
+      function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          createMapMarker(results[0]);
+        }
+      }
+
+      /*
+      pinPoster(location) takes city and fires off a Google place search
+      */
+      function pinPoster(city) {
+
+        // creates a Google place search service object. PlacesService does the work of
+        // actually searching for location data.
+        var service = new google.maps.places.PlacesService(map);
+
+          // Creates a search object for the city:
+
+          // the search request object
+          var request = {
+            query: city
+          };
+
+          // Actually searches the Google Maps API for location data and runs the callback
+          // function with the search results after each search.
+          service.textSearch(request, callback);
+        };
+
+      // Sets the boundaries of the map based on pin locations
+      window.mapBounds = new google.maps.LatLngBounds();
+
+      // pinPoster(location) creates a pin on the map for the location
+      pinPoster(location);
+      //for just the city instead of the address, use city as the parameter
+    }
+
+    initializeMap();
+
+    // Vanilla JS way to listen for resizing of the window
+    // and adjust map bounds
+    window.addEventListener('resize', function(e) {
+      //Make sure the map bounds get updated on page resize
+      map.fitBounds(mapBounds);
+    });
+
     return false;
 };
 
